a7a27c49b090478e4a0bc26ab10bfa6cdbf50021
albertgoncalves <goncalves.albert@gmail.com>
2019-08-17 11:58:51 -0400

    very important change!

--- src/main.ml
+++ src/main.ml
@@ -9,7 +9,7 @@ let promise (() : unit) : string Lwt.t = Lwt_io.read_line Lwt_io.stdin
 let rec async (() : unit) : unit Lwt.t = Lwt.bind (promise ()) callback
 
 and callback (x : string) : unit Lwt.t =
-    let _: unit Lwt.t = async () in
+    let _ : unit Lwt.t = async () in
     Unix.sleep 1;
     Lwt_io.printf "\t%s\n" x
 
