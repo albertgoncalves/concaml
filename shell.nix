972f61cbd3a145d9fb60acce0bd01c7e8b8655e7
albertgoncalves <goncalves.albert@gmail.com>
2019-11-08 21:18:44 -0500

    update ocaml version, it works!

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 mkShell {
     buildInputs = [
-        (with ocaml-ng.ocamlPackages_4_07; [
+        (with ocaml-ng.ocamlPackages_4_08; [
             findlib
             lwt4
             ocaml
